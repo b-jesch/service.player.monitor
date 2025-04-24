e1f8e3d26aa2302000c89287af200e9c22813b33
b-jesch <birger.jesch@gmail.com>
2025-04-24 15:16:07 +0200

    jsonRPC implemented

--- default.py
+++ default.py
@@ -3,6 +3,7 @@ import xbmcaddon
 import xbmcvfs
 import os
 from datetime import datetime
+import json
 
 addon = xbmcaddon.Addon()
 addon_id = addon.getAddonInfo('id')
@@ -17,6 +18,13 @@ while 0 < subversion < 100:
     log_file = os.path.join(log_path, 'monitor.log.%s' % str(subversion))
     if not xbmcvfs.exists(log_file): break
     subversion = (subversion + 1) % 100
+
+player = xbmc.Player
+
+def jsonrpc(query):
+    querystring = {"jsonrpc": "2.0", "id": 1}
+    querystring.update(query)
+    return json.loads(xbmc.executeJSONRPC(json.dumps(querystring)))
 
 
 class NotificationLogger(object):
@@ -46,20 +54,55 @@ NL = NotificationLogger()
 class EventLogger(xbmc.Monitor):
     def __init__(self):
         xbmc.Monitor.__init__(self)
+        self.playerStatus = None
+        self.lastPlayed = None
+
         NL.log('Event logger started')
         NL.log('Logging into %s' % log_file, writeout=False)
 
+    def getPlayerProps(self, data):
+        res = json.loads(data)
+        if res.get('item', None) is not None: return res['item']
+        return None
 
     def onNotification(self, sender, method, data):
-        if method == 'Player.OnPlay': NL.log('Player started: %s' % data)
+        if method == 'Player.OnPlay':
+            self.playerStatus = 'play'
+            self.lastPlayed = self.getPlayerProps(data)
+            NL.log('Player started: %s' % data)
+
         elif method == 'Player.OnPause': NL.log('Player paused: %s' % data)
         elif method == 'Player.OnResume': NL.log('Player resumed: %s' % data)
-        elif method == 'Player.OnStop': NL.log('Player stopped: %s' % data)
+
+        elif method == 'Player.OnStop':
+            self.playerStatus = 'stop'
+            self.lastPlayed = self.getPlayerProps(data)
+            NL.log('Player stopped: %s' % data)
+
         else: NL.log('Event discarded: %s' % method, writeout=False)
 
 
     def logEvents(self):
-        while not self.abortRequested(): self.waitForAbort(1000)
+        attempts = 0
+        while not self.abortRequested():
+
+            # try to restart player if status is 'stop'
+            if (self.playerStatus == 'stop' or not xbmc.getCondVisibility('Player.Playing')) and self.lastPlayed is not None:
+                if attempts < 10:
+                    attempts += 1
+                    query = {'method': 'Player.Open', 'params': {'item': {'channelid': self.lastPlayed['id']}}}
+                    res = jsonrpc(query)
+                    if 'result' in res and res['result'] == 'OK':
+                        NL.log('Player (re)opened: %s' % self.lastPlayed['title'])
+                        attempts = 0
+                    else:
+                        NL.log('Could not open Channel: %s' % self.lastPlayed['title'])
+                else:
+                    NL.log('Tried %s times to restart channel, giving up' % attempts)
+                    self.waitForAbort(30)
+            else:
+                    self.waitForAbort(30)
+
         NL.log('Event logger stopped')
 
 
